25aefe0126bfbef492684bd42f4bc21097ae2186
Marianna Korpi <marianna.korpi@tbwa.fi>
2012-11-29 10:10:59 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MariannaDemo::Application.config.secret_token = 'ec13ed325bcd76335a985edc1a723939019163117bc631d84146655204d65449b3f02be4090b055a5783b8a3ac423866ef82e8cbe2c852df708c549bf0f510df'
